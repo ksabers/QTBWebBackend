d17eed2caf5e430b4b938b51ed3e9c5fd5a71169
Giorgio Borgo <giorgio.borgo@algowatt.com>
2022-01-19 10:21:04 +0100

    Implementazione API tipi manutenzioni

---
+++ QTBWebBackend/Controllers/TipiManutenzioniController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QTBWebBackend.Interfaces;
+using System;
+
+namespace QTBWebBackend.Controllers
+{
+    public class TipiManutenzioniController : Controller
+    {
+        private ITipiManutenzioniRepository _repository;
+
+        public TipiManutenzioniController(ITipiManutenzioniRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("api/tipimanutenzioni")]
+        public IActionResult GetTipiManutenzioni()
+        {
+            return Ok(_repository.GetTipiManutenzioni());
+        }
+
+        [HttpGet("api/tipimanutenzioni/{idTipoManutenzione}")]
+        public IActionResult GetTipiManutenzioni(long idTipoManutenzione)
+        {
+            return Ok(_repository.GetTipiManutenzioni(idTipoManutenzione));
+        }
+
+        // GET: TipiManutenzioniController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipiManutenzioniController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TipiManutenzioniController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TipiManutenzioniController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TipiManutenzioniController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TipiManutenzioniController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ QTBWebBackend/Interfaces/ITipiManutenzioni.cs
@@ -0,0 +1,11 @@
+﻿using QTBWebBackend.ViewModels;
+using System.Collections.Generic;
+
+namespace QTBWebBackend.Interfaces
+{
+    public interface ITipiManutenzioniRepository
+    {
+        IEnumerable<TipoManutenzioneViewModel> GetTipiManutenzioni();
+        TipoManutenzioneViewModel? GetTipiManutenzioni(long idTipoManutenzione);
+    }
+}
